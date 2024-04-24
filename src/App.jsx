b44c56d05c6a69cd50ff17d6b059d95a1567974c
Nataliia Tsirul <tcirulnatalya@gmail.com>
2024-04-24 18:03:50 +0100

    deleted home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
-import Home from './pages/Home';
 import AboutUs from './pages/AboutUs';
 import Contact from './pages/Contact';
 import Properties from './pages/Properties';
@@ -18,7 +17,6 @@ function App() {
 <Router>
   <Routes>
     <Route path='/' element={<Layout />}>
-      <Route path='/home' element={<Home />} />
       <Route path='/aboutus' element={<AboutUs />}/>
       <Route path='/contact' element={<Contact />}/>
       <Route path='/properties' element={<Properties />}/>
--- src/components/Navbar/Header.jsx
+++ src/components/Navbar/Header.jsx
@@ -1,15 +1,12 @@
 // import React from 'react'
-import { FaCaretDown } from "react-icons/fa";
-import { GiSpookyHouse } from "react-icons/gi";
-
-
+import { FaCaretDown } from 'react-icons/fa';
+import { GiSpookyHouse } from 'react-icons/gi';
 
 const Menu = [
-  { id: 1, name: 'Home', link: '/home' },
-  { id: 2, name: 'About', link: '/aboutus' },
-  { id: 3, name: 'Properties', link: '/properties' },
-  { id: 4, name: 'Team', link: '/team' },
-  { id: 5, name: 'Contact', link: '/contact' },
+  { id: 1, name: 'About', link: '/aboutus' },
+  { id: 2, name: 'Properties', link: '/properties' },
+  { id: 3, name: 'Team', link: '/team' },
+  { id: 4, name: 'Contact', link: '/contact' },
 ];
 const DropLinks = [
   { id: 1, name: 'Houses', link: '/houses' },
@@ -22,8 +19,9 @@ function Header() {
       <div className="shadow-md bg-orange-50 ">
         <div className="container py-3 sm:py-0 flex flex-row justify-between">
           <div className="flex items-center justify-between">
-            <a href='#'><GiSpookyHouse  className="text-4xl"/>
-</a>
+            <a href="/aboutus">
+              <GiSpookyHouse className="text-4xl" />
+            </a>
           </div>
           {/* menucsection */}
           <div className="flex items-center justify-between gap-4 font-semibold">
@@ -34,16 +32,28 @@ function Header() {
                 </li>
               ))}
               <li className="group relative cursor-pointer">
-                <a href="/#home" className="flex h-[72px] items-center gap-[2px]">
+                <a
+                  href="/#home"
+                  className="flex h-[72px] items-center gap-[2px]"
+                >
                   Best Choise
                   <span>
-                      <FaCaretDown className="transition duration-300 group-hover:rotate-180" />
-                    </span>
+                    <FaCaretDown className="transition duration-300 group-hover:rotate-180" />
+                  </span>
                 </a>
                 <div className="absolute -left-9 z-[9999] hidden w-[150px] rounded-lg bg-white p-2 text-black group-hover:block shadow-md">
-                  <ul className="space-y-3">{DropLinks.map((data) => (
-                    <li key={data.id}><a href={data.link} className="inline-block w-full rounded-lg p-2 hover:bg-primary/10">{data.name}</a></li>
-                  ))}</ul>
+                  <ul className="space-y-3">
+                    {DropLinks.map((data) => (
+                      <li key={data.id}>
+                        <a
+                          href={data.link}
+                          className="inline-block w-full rounded-lg p-2 hover:bg-primary/10"
+                        >
+                          {data.name}
+                        </a>
+                      </li>
+                    ))}
+                  </ul>
                 </div>
               </li>
             </ul>
